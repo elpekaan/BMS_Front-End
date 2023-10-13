a42aff387a40b33f37a52f96d09b2f34aafa2928
Alihan Tayçu <alihan.taycu@isik.edu.tr>
2023-10-13 16:30:06 +0300

    Update Project Parts.

--- src/core/models/project.model.ts
+++ src/core/models/project.model.ts
@@ -2,10 +2,10 @@ export class Project {
     id: number = 0;
     userId: number = 0;
     roomId: number = 0;
-    projectTitle: string = ''; 
-    projectDescription: string = '';
-    projectStatus: ProjectStatus = 0;
-    projectDeadline: Date = new Date(); // Hata çıkarsa backendi düzelt. 
+    project_Title: string = "";
+    project_Description: string = '';
+    project_Status: ProjectStatus = 0;
+    project_Deadline: string =""; // Hata çıkarsa backendi düzelt.
     // string de verebilirsin.
 }
 
