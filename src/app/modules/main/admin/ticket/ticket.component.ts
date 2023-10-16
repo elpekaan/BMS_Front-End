d9819fcb7ae2dd78ca8140e9c5fba2d3107a31f0
Alihan Tayçu <alihan.taycu@isik.edu.tr>
2023-10-16 14:31:30 +0300

    Updated Task and Ticket.

--- src/app/modules/main/admin/ticket/ticket.component.ts
+++ src/app/modules/main/admin/ticket/ticket.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/core/services/api/api.service';
 import { Ticket } from 'src/core/models/ticket.model';
-
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.scss']
 })
-export class TicketComponent implements OnInit{
+export class TicketComponent implements OnInit {
 
   constructor(private apiService: ApiService) {}
-  
+
   tickets: Ticket[] = [];
 
   ngOnInit() {
     this.apiService
       .getAllEntities(Ticket)
       .subscribe((result) => {
-        this.tickets = result.data; 
+        this.tickets = result.data;
         console.log(result.data);
       });
   }
