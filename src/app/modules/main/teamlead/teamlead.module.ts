6a6e69e1dfd6dcc1cdd039b5f49996026d7ce800
Alihan Tayçu <alihan.taycu@isik.edu.tr>
2023-10-26 13:55:26 +0300

    TeamLead Pages Updated

--- src/app/modules/main/teamlead/teamlead.module.ts
+++ src/app/modules/main/teamlead/teamlead.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { TeamLeadDashboardComponent } from './dashboard/dashboard.component';
 import { TeamLeadProjectComponent } from './project/project.component';
 import { TeamLeadRoomComponent } from './room/room.component';
 import { TeamLeadTaskComponent } from './task/task.component';
 import { TeamLeadTicketComponent } from './ticket/ticket.component';
-
+import { FormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 
 @NgModule({
@@ -17,7 +20,11 @@ import { TeamLeadTicketComponent } from './ticket/ticket.component';
     TeamLeadTicketComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ToastModule
   ]
 })
 export class TeamleadModule { }
